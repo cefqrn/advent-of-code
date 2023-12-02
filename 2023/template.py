7a271a3fd795cd57df54c3a32291a88b5182dd0d
Cefqrn <cefqrn@gmail.com>
2023-12-01 20:04:44 -0500

    2023 updated template

--- 2023/template.py
+++ 2023/template.py
@@ -1,7 +1,11 @@
+from contextlib import suppress
+from itertools import groupby
 from pathlib import Path
 
 with open(Path(__file__).parent / "input") as f:
     data = f.read().rstrip()
 
-blocks = data.split("\n\n")
-ints = list(map(int, data.split()))
+lines = data.splitlines()
+blocks = [tuple(group) for has_content, group in groupby(lines, bool) if has_content]
+with suppress(ValueError):
+    ints = [int(x) for x in data.split()]
