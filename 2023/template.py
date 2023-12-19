4ec3b79fa6466e67ca5d97a748d6870d8586ee3a
Cefqrn <cefqrn@gmail.com>
2023-12-19 00:57:42 -0500

    2023 updated template

--- 2023/template.py
+++ 2023/template.py
@@ -1,11 +1,34 @@
+from collections import Counter, defaultdict, deque
 from contextlib import suppress
-from itertools import groupby
+from functools import cache, partial
+from itertools import batched, chain, cycle, count, combinations, groupby, islice, pairwise, permutations, product, repeat, starmap, tee
+from operator import eq, gt, lt, ge, le, add, sub, mul
 from pathlib import Path
+from bisect import bisect, bisect_left, insort, insort_left
+from heapq import heappop, heappush
+from math import ceil, dist, floor, gcd, inf, lcm, prod, sqrt
+from sys import setrecursionlimit
+from re import findall, finditer, search
 
-with (Path(__file__).parent / "input").open() as f:
-    data = f.read().rstrip()
+try:
+    with (Path(__file__).parent / "input").open() as f:
+        data = f.read().rstrip()
+except FileNotFoundError as e:
+    print(e)
+    data = " "
 
 lines = data.splitlines()
 blocks = [tuple(group) for has_content, group in groupby(lines, bool) if has_content]
-with suppress(ValueError):
-    ints = [int(x) for x in data.split()]
+
+COMPLEX_COORDINATES = False
+
+# NESW URDL
+directions = [
+     (-1,  0), -1 + 0j,
+     ( 0,  1),  0 + 1j,
+     ( 1,  0),  1 + 0j,
+     ( 0, -1),  0 - 1j
+][COMPLEX_COORDINATES::2]
+
+height = len(lines)
+width = len(lines[0])
