25c332ef6afdf9ef89d8ac086c7b5154b95ffa2c
Cefqrn <cefqrn@gmail.com>
2024-12-13 23:55:12 -0500

    modified template

--- 2024/template.py
+++ 2024/template.py
@@ -1,14 +1,45 @@
+from itertools import groupby, product
 from pathlib import Path
+
+p1 = p2 = 0
 
 INPUT_FILE = Path(__file__).parent / "input"
 
 contents = INPUT_FILE.read_text().rstrip()
-lines = contents.split("\n")
-sections = contents.split("\n\n")
+lines = contents.splitlines()
+sections = [
+    list(section)
+    for has_content, section in groupby(lines, bool)
+    if has_content
+]
 
-p1 = p2 = 0
+grid = {}
+ipos = None
+for y, line in enumerate(lines):
+    for x, c in enumerate(line):
+        grid[x, y] = c
+        if c == '':
+            ipos = x, y
 
-print(lines)
+w, h = len(line), len(lines)
+
+
+
+
+
+
+
+
+
+
+for pos in product(range(w), range(h)):
+    x, y = pos
+
+for section in sections:
+    section
+
+for line in lines:
+    line
 
 print(p1)
 print(p2)
