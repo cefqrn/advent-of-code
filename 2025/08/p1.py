8b3071fcab8dc4d8d287a9f3b0e31cb468a1698c
cefqrn <cefqrn@gmail.com>
2025-12-08 00:14:41 -0500

    08

---
+++ 2025/08/p1.py
@@ -0,0 +1,52 @@
+from bisect import bisect_left, bisect_right
+from collections import Counter, deque, defaultdict
+from fractions import Fraction
+from functools import cache, partial
+from itertools import chain, cycle, islice, pairwise, combinations, product
+from heapq import heappop, heappush
+from math import inf, dist, prod
+from re import findall, match
+
+from dsu import DisjointSetUnion
+
+directions = (0, 1), (1, 0), (0, -1), (-1, 0)
+
+def parse(s):
+    result = []
+    for l in s.splitlines():
+        result.append(eval(l))
+
+
+    return result
+
+
+def solve(data):
+    result = 0
+
+    connected = DisjointSetUnion(len(data))
+
+    possible = list(combinations(range(len(data)), 2))
+    possible.sort(key=lambda x: dist(data[x[0]], data[x[1]]))
+
+    # print(possible)
+    connection_count = 0
+    for a, b in possible:
+        connected.union(a, b)
+        connection_count += 1
+        if connection_count == 1000:
+            biggest = sorted(map(len, connected.to_sets()))[-3:]
+            return prod(biggest)
+
+
+
+
+
+
+
+    return result
+
+if __name__ == "__main__":
+    with open("input") as f:
+        data = parse(f.read().rstrip("\n"))
+
+    print(solve(data))
